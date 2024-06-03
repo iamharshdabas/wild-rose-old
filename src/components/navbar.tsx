1cff894b20886ca832192ff8f54c27296c49d0bf
Harsh Dabas <iamharshdabas@gmail.com>
2024-06-03 15:41:20 +0530

    fix: navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,8 +9,9 @@ import {
   NavbarMenuToggle,
   Navbar as NextUINavbar,
 } from "@nextui-org/navbar";
-import { link as linkStyles } from "@nextui-org/theme";
 import clsx from "clsx";
+import { NavLink } from "react-router-dom";
+import { link } from "@nextui-org/theme";
 
 import { LogoIcon } from "./icons";
 
@@ -35,16 +36,18 @@ export const Navbar = () => {
         <div className="hidden sm:flex gap-4 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
             <NavbarItem key={item.href}>
-              <Link
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
-                )}
+              <NavLink
+                className={({ isActive }) =>
+                  clsx(
+                    link({ color: "foreground" }),
+                    isActive ? "font-medium text-primary" : "",
+                  )
+                }
                 color="foreground"
-                href={item.href}
+                to={item.href}
               >
                 {item.label}
-              </Link>
+              </NavLink>
             </NavbarItem>
           ))}
         </div>
@@ -63,20 +66,21 @@ export const Navbar = () => {
       <NavbarMenu>
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 0
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
+            <NavbarMenuItem key={`${item.label}-${item.href}`}>
+              <NavLink
+                className={({ isActive }) =>
+                  clsx(
+                    index === siteConfig.navMenuItems.length - 1
+                      ? link({ color: "danger" })
+                      : link({ color: "foreground" }),
+                    isActive ? "font-medium text-primary" : "",
+                  )
                 }
-                href={item.href}
-                size="lg"
+                color="foreground"
+                to={item.href}
               >
                 {item.label}
-              </Link>
+              </NavLink>
             </NavbarMenuItem>
           ))}
         </div>
